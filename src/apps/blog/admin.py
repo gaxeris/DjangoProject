2f929a80bf9eae6aafb173c87392737b1538402b
gaxeris <andreykardanev@gmail.com>
2024-05-06 20:55:29 +0300

    автозаполнение полей в админке

--- src/apps/blog/admin.py
+++ src/apps/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from apps.blog.models import Category, Post
+
 # Register your models here.
+
+# генерация url при создании записей в админке
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
