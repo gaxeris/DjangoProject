8d79e544673d4f5fd98c656b32bfdb9b305dfb0b
gaxeris <andreykardanev@gmail.com>
2024-05-09 13:52:50 +0300

    fix: swapping the urls
    забыл что оно останавливает проверку на первом совпавшем паттерне
    и оно воспринимало create за <slug: ...>

--- src/apps/blog/urls.py
+++ src/apps/blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'apps.blog'
 urlpatterns = [
     path('', views.index, name='blog'),
     path('categories/', views.display_all_categories, name='categories'),
+    path('posts/create/', views.create_new_post),
     path('posts/<slug:url>/', views.get_post_by_url, name='single-post'),
-    path('posts/create/', views.create_new_post)
 ]
