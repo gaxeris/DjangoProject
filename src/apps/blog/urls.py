1f4b19cbe8303aed6a41042837a28d39fe465f47
gaxeris <andreykardanev@gmail.com>
2024-05-09 13:49:18 +0300

    fix: trailing underscore

--- src/apps/blog/urls.py
+++ src/apps/blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='blog'),
     path('categories/', views.display_all_categories, name='categories'),
     path('posts/<slug:url>/', views.get_post_by_url, name='single-post'),
-    path('posts/create', views.create_new_post)
+    path('posts/create/', views.create_new_post)
 ]
