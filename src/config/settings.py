94e00c62fd09b142f5495d82b83ec8a7948d6386
gaxeris <andreykardanev@gmail.com>
2024-05-10 15:26:27 +0300

    feat: installing cors for future testing of the API on the frontend side

--- src/config/settings.py
+++ src/config/settings.py
@@ -39,12 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'corsheaders',
     
     'apps.main.apps.MainConfig',
     'apps.blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
