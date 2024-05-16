e731cf7d92b09f730f49fe5b1957b368bd807efb
gaxeris <andreykardanev@gmail.com>
2024-05-16 15:24:43 +0300

    fiкс: renamed wrong field back to normal

--- frontend/src/components/todos/TodoItem.jsx
+++ frontend/src/components/todos/TodoItem.jsx
@@ -6,8 +6,12 @@ function TodoItem({ todo }) {
     return (
         <div className="todo-container">
             <p className="todo-title">{todo.title}</p>
-            <p className="todo-description">{todo.content}</p>
+            <p className="todo-description">{todo.description}</p>
 
+
+            <button className="delete-button" onClick={() => onDelete(todo.id)}>
+                Delete
+            </button>
         </div>
     );
 }
