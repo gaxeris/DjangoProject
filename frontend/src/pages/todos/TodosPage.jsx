6d216bb76e0cf497e2cd37f675444f6257eb2b3c
gaxeris <andreykardanev@gmail.com>
2024-05-16 15:25:23 +0300

    new: implemented aксios GET, POST and DELETE requests

--- frontend/src/pages/todos/TodosPage.jsx
+++ frontend/src/pages/todos/TodosPage.jsx
@@ -1,28 +1,87 @@
-import react, { useState, useEffect } from "react"
-import TodoItem from '../../components/todos/TodoItem'
+import react, { useState, useEffect } from "react";
+import axios from "axios";
+import TodoItem from '../../components/todos/TodoItem';
 
 function TodosPage() {
 
-  const [todoItems, setTodoItems] = useState([])
+  const [todoItems, setTodoItems] = useState([]);
+
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
+
 
   useEffect(() => {
-    fetchTodoItems()
+    getTodoItems()
   }, []);
 
-  const fetchTodoItems = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/todos/");
-    const data = await response.json();
-    setTodoItems(data);
-    console.log(data)
+
+  function getTodoItems() {
+    return axios.get('http://127.0.0.1:8000/api/todos/')
+    .then((res) => res.data)
+    .then((data) => {
+        setTodoItems(data);
+        console.log(data);
+    })
+    .catch((err) => alert(err));
+  }
+
+  const deleteTodoItems = (id) => {
+    return axios.delete(`http://127.0.0.1:8000/api/todos/${id}/`)
+      .then((res) => {
+          if (res.status === 204) alert("Todo item deleted!");
+          else alert("Failed to delete the todo item.");
+          getTodoItems();
+      })
+      .catch((error) => alert(error));
   };
+
+  const createTodoItems = (e) => {
+    e.preventDefault();
+    return axios.post("http://127.0.0.1:8000/api/todos/", { description, title })
+        .then((res) => {
+            if (res.status === 201) alert("Todo item created!");
+            else alert("Failed to make a todo item.");
+            getTodoItems();
+        })
+        .catch((err) => alert(err));
+  };
+
 
   return (
     <>
       <div>
         <h2>Todo Items</h2>
-        {todoItems.map((todo) => (
-          <TodoItem todo={todo} />
-        ))}
+        <ul>
+          {todoItems.map((todo) => (
+            <li><TodoItem todo={todo} onDelete={deleteTodoItems} key={todo.id} /></li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Create a Todo Item</h2>
+          <form onSubmit={createNote}>
+            <label htmlFor="title">Title:</label>
+            <br />
+            <input
+                type="text"
+                id="title"
+                name="title"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+            <label htmlFor="content">Description:</label>
+            <br />
+            <textarea
+                id="content"
+                name="content"
+                required
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+            <br />
+            <input type="submit" value="Submit"></input>
+          </form>
       </div>
     </>
   )
