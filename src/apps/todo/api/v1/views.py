e7dbdfdc2ce92b61094ff89be0b69bd5f9f94cd2
gaxeris <andreykardanev@gmail.com>
2024-05-22 23:32:51 +0300

    new: updating todo list to isplay only to its author

--- src/apps/todo/api/v1/views.py
+++ src/apps/todo/api/v1/views.py
@@ -1,5 +1,6 @@
-from urllib import request
+
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from apps.todo.models import TodoItem
 from apps.todo.api.v1.serializers import TodoItemSerializer
@@ -9,6 +10,7 @@ from apps.todo.api.v1.serializers import TodoItemSerializer
 class TodoItemViewSet(viewsets.ModelViewSet):
     model = TodoItem
     serializer_class = TodoItemSerializer
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         author = self.request.user
