3ea07f27df8253d38ea6217f8755042f44453eac
gaxeris <andreykardanev@gmail.com>
2024-05-23 19:39:44 +0300

    new: forgotten url namespace

--- src/apps/blog/api/v1/urls.py
+++ src/apps/blog/api/v1/urls.py
@@ -7,6 +7,7 @@ from apps.blog.api.v1.views import AuthorsViewSet, CategoryViewSet, PostViewSet
 
 router_blog = routers.DefaultRouter()
 
+app_name = 'blog'
 
 router_blog.register(r'posts', PostViewSet)
 router_blog.register(r'categories', CategoryViewSet)
