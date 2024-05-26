08f3da896d5c45af868822519a03905fedc71d17
gaxeris <andreykardanev@gmail.com>
2024-05-26 23:40:41 +0300

    chore: model fields params

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -10,7 +10,10 @@ class User(AbstractUser):
     """Custom user model which replaces Django\`s auth.User and inherits the same behavior"""
 
     image = models.ImageField(
-        upload_to=user_self_directory_path, width_field=100, height_field=100, null=True
+        upload_to=user_self_directory_path,
+        width_field=None,
+        height_field=None,
+        null=True,
     )
 
     class Meta:
