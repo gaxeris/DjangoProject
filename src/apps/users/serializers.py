bc1e34113667dfefc2155d4ee9a8f564d45aed72
gaxeris <andreykardanev@gmail.com>
2024-05-26 23:39:59 +0300

    new: eхtracting image addition endpoint to ..../image/ route

--- src/apps/users/serializers.py
+++ src/apps/users/serializers.py
@@ -1,12 +1,33 @@
+from time import sleep
 from rest_framework import serializers
-
+from django.core.files import File
+from django.core.files.storage import FileSystemStorage
 from apps.users.models import User
+from tasks.tasks import upload
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "image", "username"]
+
+
+class UserImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "image"]
+        extra_kwargs = {"partial": True}
+
+    def update(self, instance, validated_data):
+        image_file = self.context["image_file"]
+        storage = FileSystemStorage()
+        storage.save(image_file.name, File(image_file))
+
+        return upload.delay(
+            id=instance.pk,
+            path=storage.path(image_file.name),
+            file_name=image_file.name,
+        )
 
 
 class UserLoginSignupSerializer(serializers.ModelSerializer):
--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,9 +1,14 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 from apps.users.models import User
-from apps.users.permissions import UserCustomPermission
-from apps.users.serializers import UserLoginSignupSerializer, UserSerializer
+from apps.users.permissions import UserIsSelfPermission
+from apps.users.serializers import (
+    UserImageSerializer,
+    UserLoginSignupSerializer,
+    UserSerializer,
+)
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -18,7 +23,29 @@ class UserListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
 
-class UserRetrieveView(generics.RetrieveAPIView, generics.UpdateAPIView):
+class UserRetrieveView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, UserCustomPermission]
+    permission_classes = [IsAuthenticated]
+
+
+class UserSetImageView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserImageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        image_file = self.request.FILES["image"]
+        serializer = UserImageSerializer(
+            instance=instance,
+            data=request.data,
+            context={"image_file": image_file},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response("Upload Started...")
+
+    def put(self, request, *args, **kwargs):
+        return self.patch(request, *args, **kwargs)
