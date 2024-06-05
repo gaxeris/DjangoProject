c063d06fcef03b5d900cd67d5bc0ba43a1364bad
gaxeris <andreykardanev@gmail.com>
2024-06-05 22:04:35 +0300

    new: now django container launches through entrypoint, while celery from the same image launches through command

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,9 +1,16 @@
-#!/bin/sh for docker container
+#!/bin/sh
 
 
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 #DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --username admin --email admin@example.ru --noinput
+
+#without this check celery throws a psycopg2 error despite connecting to Redis container
+echo "Waiting for PostgreSQL"
+while ! nc -z $DATABASE_HOST $DATABASE_PORT; do 
+    sleep 0.1
+done
+echo "PostgreSQL is ready"
 
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
